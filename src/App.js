f030089c9217914f528d3c84c39c8da5db67ff10
Rutu83 <rathodrutu1996@gmail.com>
2022-07-22 17:28:49 +0530

    Front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [data,addData] = useState([])
+  const [name,updateName] = useState("")
+
+  useEffect(() => {
+    fetch("https://localhost:44383/api/Cu")
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+      if (data.length === 0){
+        addData(res)
+      }
+    })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' placeholder='name' onChange={e => updateName(e.target.value) }></input>
+      
+ 
+      {
+        data.map(d => {
+        
+          if (d.customerName.includes(name)){
+          return(
+            <div className='xd'>
+             
+              <h3>{d.customerId}</h3>
+              <h3>{d.customerName}</h3>
+              <h3>{d.address}</h3>
+              <h3>{d.city}</h3>
+             
+            </div> 
+          )
+          }
+        })
+      }
+    
+       
     </div>
   );
+
+  
 }
 
 export default App;
